a9357f242e52054f306b1f6d49461fe86a05730f
187****8950 <891515441@qq.com>
2025-09-26 07:16:33 +0000

    调整了顺序表的代码！

--- CLanguage/SeqList.cpp
+++ CLanguage/SeqList.cpp
@@ -2,26 +2,38 @@
 #include <iostream>
 using namespace std;
 
-#define INIT_SIZE 5
 typedef int ElemType;
 struct List
 {
-    int length; //元素实际长度
-    int listsize;
-    ElemType * elem;  //存储空间的首地址
+    int length;   //元素实际长度
+    int maxsize;  //顺序表的最大容量
+    ElemType * element;  //存储空间的首地址
 };
 
-//顺序表的初始化
-int InitList(List* list)
+/*函数功能：顺序表的初始化
+ *函数形参：顺序表的最大元素容量，int maxsize
+ *函数返回值：初始化成功返回实际地址，否则返回空，List*类型
+*/
+List* InitList(int maxsize)
 {
-    list->elem = new ElemType[INIT_SIZE];
-    if(list->elem)
+    List* list = new List;  //申请一个顺序表
+    if(list)  //顺序表申请成功
     {
-        list->listsize = INIT_SIZE;
-        list->length = 0;
-        return 1;
+        list->element = new ElemType[maxsize];
+        if(list->element)
+        {
+            list->length = 0;
+            list->maxsize = maxsize;
+            return list;
+        }
+        else
+        {
+            cout << "Initialize the 'element' member failed."<<endl;
+            delete list;
+            return nullptr;        
+        }
     }
-    return 0;
+    return nullptr;
 }
 
 
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # DataStructure
-This program is designed for education about data structure, which include link-list, stack, queue, bianry-tree etc.
+1. This program is designed for education about data structure, which include link-list, stack, queue, bianry-tree etc.
+2. Please note that the source code may contain bugs and intended for reference purposes only.
